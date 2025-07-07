45e61bbd32496918f65d00116f7843e269506ceb
Sotatek-PhongBui <phong.bui@sotatek.com>
2025-07-07 12:56:35 +0700

    feat: update payment

--- payment-service/src/app.service.ts
+++ payment-service/src/app.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { RedisClientType } from 'redis';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
@@ -7,6 +8,7 @@ export class AppService {
     @Inject('REDIS_PUBLISHER') private readonly redisPublisher: RedisClientType,
     @Inject('REDIS_SUBSCRIBER')
     private readonly redisSubscriber: RedisClientType,
+    private readonly configService: ConfigService,
   ) {}
   getHello(): string {
     return 'Hello World!';
@@ -15,24 +17,27 @@ export class AppService {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
   async onModuleInit() {
-    await this.redisSubscriber.subscribe('order.created', async (message) => {
-      console.log(
-        `order created at ${new Date().toISOString()}`,
-        JSON.parse(message),
-      );
-      interface Order {
-        id: string;
-        status: string;
-      }
-      const order = JSON.parse(message) as Order;
-      await this.delay(Math.random() * 60000);
-      const result = Math.random() > 0.5 ? 'confirmed' : 'declined';
-      order.status = result;
-      console.log(`payment verified at ${new Date().toISOString()}}`, order);
-      await this.redisPublisher.publish(
-        'payment.verified',
-        JSON.stringify(order),
-      );
-    });
+    await this.redisSubscriber.subscribe(
+      this.configService.get('ORDER_CREATED', 'order.created'),
+      async (message) => {
+        console.log(
+          `order created at ${new Date().toISOString()}`,
+          JSON.parse(message),
+        );
+        interface Order {
+          id: string;
+          status: string;
+        }
+        const order = JSON.parse(message) as Order;
+        await this.delay(Math.random() * 60000);
+        const result = Math.random() > 0.5 ? 'confirmed' : 'declined';
+        order.status = result;
+        console.log(`payment verified at ${new Date().toISOString()}}`, order);
+        await this.redisPublisher.publish(
+          this.configService.get('PAYMENT_VERIFIED', 'payment.verified'),
+          JSON.stringify(order),
+        );
+      },
+    );
   }
 }
--- payment-service/src/main.ts
+++ payment-service/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4002);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT', 4002));
   console.log('payment service is listening on port 4002');
 }
 bootstrap();
